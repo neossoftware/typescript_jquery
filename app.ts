6ca316aa4857c7072656dc6d8494256e52f27912
neossoftware <mario.hidalgo@yahoo.com.mx>
2018-07-13 19:58:20 -0500

    ejemplo typescript + jquery

--- app.ts
+++ app.ts
@@ -7,10 +7,21 @@ class InvoiceTemplate {
             });
         });
     }
+
+     onClickButton() {
+        console.log('Boton presionado');
+    }
 }
  
 window.onload = () => {
-    var el = document.getElementById('content');
+   
+    //var el = document.getElementById('content');
+    let el = $('#content')[0];
     var example = new InvoiceTemplate(el);
+    $('#otro').append("cadena");
+    $('#btnAceptar').on("click", function() {
+       example.onClickButton();
+    });
+   
  
 };
